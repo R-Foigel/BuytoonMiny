80659e7b8215d2ea78fafaf24145a3b9f9329d64
rudy <rudif@sgbb.org.il>
2026-05-07 19:13:23 +0300

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+import pyodbc
+
+server = 'localhost'
+database = 'beton'
+username = 'tichnut'
+password = '1234'
+# 1. הגדר את פרטי החיבור שלך
+server = 'localhost'  # או 'localhost\SQLEXPRESS' אם זה SQL Express
+database = 'beton'
+username = 'tichnut'  # שם המשתמש שלך ב-SQL Server
+password = 'הסיסמה שלך כאן'
 
 
-print("Hello Worldd")
+
+
+
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=beton;Trusted_Connection=yes;'
+)
+cursor = conn.cursor()
+# שאילתה לשליפת כל הלקוחות
+cursor.execute("SELECT id, name, phone FROM Customers")
+
+# שליפת כל השורות
+rows = cursor.fetchall()
+
+# הדפסת התוצאות
+for row in rows:
+    print(f"ID: {row.id}, Name: {row.name}, Phone: {row.phone}")
